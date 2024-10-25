8de8045e74bebf3fbbd66dfa6af61d41f6a332eb
Karenzinha12345 <karenpintanelli@gmail.com>
2024-10-25 14:44:52 -0300

    schimdit page home

--- src/components/Routes/Home/Home.jsx
+++ src/components/Routes/Home/Home.jsx
@@ -41,151 +41,117 @@ export const Home = () => {
       </div>
 
       <div className="services">
-      <div className="card">
-          <div className="ionicon">
-           
-          </div>
+        <div className="card">
+          <div className="ionicon"></div>
+          <img src="src/assets/hb20.png"></img>
           <div className="texts">
             <h2>JEEP RENEGADE</h2>
-            <p>
-            2.0 16V TURBO DIESEL TRAILHAWK 4P 4X4 AUTOMÁTICO
-            </p>
-          </div>
-        </div>
-
-      <div className="card">
-          <div className="ionicon">
-           
-          </div>
-          <div className="texts">
-            <h2>JEEP RENEGADE</h2>
-            <p>
-            2.0 16V TURBO DIESEL TRAILHAWK 4P 4X4 AUTOMÁTICO
-            </p>
+            <p>2.0 16V TURBO DIESEL TRAILHAWK 4P 4X4 AUTOMÁTICO</p>
           </div>
         </div>
 
         <div className="card">
-          <div className="ionicon">
-           
-          </div>
+          <div className="ionicon"></div>
+          <img src="src/assets/JETTA-removebg-preview.png"></img>
           <div className="texts">
             <h2>JEEP RENEGADE</h2>
-            <p>
-            2.0 16V TURBO DIESEL TRAILHAWK 4P 4X4 AUTOMÁTICO
-            </p>
+            <p>2.0 16V TURBO DIESEL TRAILHAWK 4P 4X4 AUTOMÁTICO</p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="ionicon"></div>
+          <img src="src/assets/golf-removebg-preview.png"></img>
+          <div className="texts">
+            <h2>JEEP RENEGADE</h2>
+            <p>2.0 16V TURBO DIESEL TRAILHAWK 4P 4X4 AUTOMÁTICO</p>
           </div>
         </div>
 
         <div className="card">
           <div className="ionicon">
-           
-            <img src="src/assets/png-clipart-fiat-strada-fiat-automobiles-car-fiat-idea-fiat-siena-car-compact-car-truck-thumbnail.png" ></img>
+            <img src="src/assets/images-removebg-preview.png"></img>
           </div>
           <div className="texts">
             <h2>FIAT STRADA</h2>
-            <p>
-            1.3 FIREFLY FLEX RANCH CD CVT
-            </p>
+            <p>1.3 FIREFLY FLEX RANCH CD CVT</p>
           </div>
         </div>
         <div className="card">
-          <div className="ionicon">
-          </div>
+          <div className="ionicon"></div>
+          <img src="src/assets/van verde.png"></img>
           <div className="texts">
             <h2>FIAT STRADA</h2>
-            <p>
-            1.3 FIREFLY FLEX RANCH CD CVT
-            </p>
+            <p>1.3 FIREFLY FLEX RANCH CD CVT</p>
           </div>
         </div>
         <div className="card">
-          <div className="ionicon">
-          
-          </div>
+          <div className="ionicon"></div>
+          <img src="src/assets/van preta.png"></img>
           <div className="texts">
             <h2>FIAT STRADA</h2>
-            <p>
-            1.3 FIREFLY FLEX RANCH CD CVT
-            </p>
+            <p>1.3 FIREFLY FLEX RANCH CD CVT</p>
           </div>
         </div>
         <div className="card">
-          <div className="ionicon">
-           
-          </div>
+          <div className="ionicon"></div>
+          <img src="src/assets/van branca.png"></img>
           <div className="texts">
             <h2>FIAT STRADA</h2>
-            <p>
-            1.3 FIREFLY FLEX RANCH CD CVT
-            </p>
+            <p>1.3 FIREFLY FLEX RANCH CD CVT</p>
           </div>
         </div>
         <div className="card">
-          <div className="ionicon">
-           
-          </div>
+          <div className="ionicon"></div>
+          <img src="src/assets/van azul.png"></img>
           <div className="texts">
             <h2>FIAT STRADA</h2>
-            <p>
-            1.3 FIREFLY FLEX RANCH CD CVT
-            </p>
+            <p>1.3 FIREFLY FLEX RANCH CD CVT</p>
           </div>
         </div>
         <div className="card">
-          <div className="ionicon">
-           
-          </div>
+          <div className="ionicon"></div>
+
+
           <div className="texts">
             <h2>FIAT STRADA</h2>
-            <p>
-            1.3 FIREFLY FLEX RANCH CD CVT
-            </p>
+            <p>1.3 FIREFLY FLEX RANCH CD CVT</p>
           </div>
         </div>
         <div className="card">
+          <div className="ionicon"></div>
+
+
+          <div className="texts">
+            <h2>FIAT STRADA</h2>
+            <p>1.3 FIREFLY FLEX RANCH CD CVT</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="ionicon"></div>
+
+
+          <div className="texts">
+            <h2>FIAT STRADA</h2>
+            <p>1.3 FIREFLY FLEX RANCH CD CVT</p>
+          </div>
+        </div>
+
+        <div className="card">
           <div className="ionicon">
+
             
+            <div className="texts">
+              <h2>FIAT STRADA</h2>
+              <p>1.3 FIREFLY FLEX RANCH CD CVT</p>
+            </div>
           </div>
-          <div className="texts">
-            <h2>FIAT STRADA</h2>
-            <p>
-            1.3 FIREFLY FLEX RANCH CD CVT
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="ionicon">
-           
-          </div>
-          <div className="texts">
-            <h2>FIAT STRADA</h2>
-            <p>
-            1.3 FIREFLY FLEX RANCH CD CVT
-            </p>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="ionicon">
-          <div className="texts">
-            <h2>FIAT STRADA</h2>
-            <p>
-            1.3 FIREFLY FLEX RANCH CD CVT
-            </p>
-          </div>
-          
-          </div>
-          
         </div>
       </div>
 
-      
-
-        {/* <div className="col-12 col-md-2 d-flex flex-column flex-md-row align-items-center justify-content-center"> */}
-        {/* </div> */}
-      </div>
-    
+      {/* <div className="col-12 col-md-2 d-flex flex-column flex-md-row align-items-center justify-content-center"> */}
+      {/* </div> */}
+    </div>
   );
 };
 
